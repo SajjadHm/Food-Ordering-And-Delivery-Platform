f69a57855fe5b46cd4af5538a0556bdd4d11411d
Parsa <mparsasadeghiofficial@gmail.com>
2023-05-21 11:41:49 +0430

    Commit #002 - LoginMenuMessages
    - LoginMenuMessages enum added in view.enums.

---
+++ view/enums/LoginMenuMessages.java
@@ -0,0 +1,34 @@
+package view.enums;
+
+public enum LoginMenuMessages {
+    INCORRECT_USERNAME("Incorrect Username!"
+            + "\n" + "Username doesn't contain spaces."),
+    INCORRECT_PASSWORD("Incorrect Password!"
+            + "\n" + "Password doesn't contain spaces."),
+    WEAK_PASSWORD("Weak Password!"
+            + "\n" + "Password must be at least 8 characters long and contains both " +
+            "upper-case and lower-case letters, numbers and special characters."),
+    INVALID_ADMIN_USERNAME("Invalid Admin Username!"
+            + "\n" + "There is no admin account with this username."),
+    INVALID_USER_USERNAME("Invalid User Username!"
+            + "\n" + "There is no user account with this username."),
+    INVALID_PASSWORD("Invalid Password"
+            + "\n" + "Passwords doesn't match."),
+    ADMIN_ACCOUNT_CREATED("Admin account created successfully."),
+    USER_ACCOUNT_CREATED("User account created successfully."),
+    ADMIN_LOGIN_SUCCESSFUL("Admin account logged in successfully."),
+    USER_LOGIN_SUCCESSFUL("User account logged in successfully."),
+    LOGOUT("Logged Out"),
+    INVALID_COMMAND("Inavlid Command!"),
+    ;
+
+    final String message;
+
+    LoginMenuMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
