35d7ca94b2f8498aadeee26d67935e1860ea8ebe
Parsa <mparsasadeghiofficial@gmail.com>
2023-05-22 15:09:47 +0430

    Commit #014 - Resturant (Food Type)
    - Resturant class edited.
          getters and setters
    - Admin class edited.
          resturants ArrayList
          getters and setters

--- model/resturant/Resturant.java
+++ model/resturant/Resturant.java
@@ -5,7 +5,7 @@ import model.enums.ResturantFoodType;
 public class Resturant {
     private String name;
     private ResturantFoodType foodType;
-    private int id;
+    private final int id;
     private static int idCount;
 
     static {
@@ -17,6 +17,31 @@ public class Resturant {
         this.foodType = foodType;
         this.id = idCount;
         idCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ResturantFoodType getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(ResturantFoodType foodType) {
+        // TODO: empty foods menu
+        this.foodType = foodType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static int getIdCount() {
+        return idCount;
     }
 
 }
