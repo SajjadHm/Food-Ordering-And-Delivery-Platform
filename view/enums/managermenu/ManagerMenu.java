e94e67bfe9cacf5c8be236975a27c09381f1c378
Parsa <mparsasadeghiofficial@gmail.com>
2023-05-27 11:46:51 +0430

    Commit #033 - add restaurant
    - add restaurant command implied.

--- view/enums/managermenu/ManagerMenu.java
+++ view/enums/managermenu/ManagerMenu.java
@@ -1,4 +1,38 @@
 package view.enums.managermenu;
 
+import controllers.ManagerMenuController;
+import model.enums.ResturantFoodType;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
 public class ManagerMenu {
+    private static boolean isRunning;
+    private static Matcher matcher;
+    private static String input;
+    private static ManagerMenuMessages message;
+
+    static {
+        isRunning = false;
+    }
+
+    public static ManagerMenuResults run(Scanner scanner) {
+        isRunning = true;
+        while (isRunning) {
+            message = ManagerMenuMessages.INVALID_COMMAND;
+            input = scanner.nextLine().trim();
+            if ((matcher = ManagerMenuCommands.getMatcher(input, ManagerMenuCommands.END)) != null)
+                return ManagerMenuResults.END;
+            if ((matcher = ManagerMenuCommands.getMatcher(input, ManagerMenuCommands.ADD_RESTAURANT)) != null)
+                message = checkAddRestaurant();
+        }
+        return null;
+    }
+
+    public static ManagerMenuMessages checkAddRestaurant() {
+        String name = matcher.group("name");
+        ResturantFoodType[] foodType = ResturantFoodType.getType(matcher.group("foodType").split("\\s+"));
+        String location = matcher.group("location");
+        return ManagerMenuController.checkAddRestaurant(name, location, foodType);
+    }
 }
