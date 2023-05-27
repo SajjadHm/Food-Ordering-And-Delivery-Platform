0cca31d210403adb1a4fa6d802293a50e3dd83fe
Parsa <mparsasadeghiofficial@gmail.com>
2023-05-27 11:10:32 +0430

    Commit #031 - ManagerMenuMessages
    - ManagerMenuMessages enum added in view.enums.managermenu.

--- view/enums/managermenu/ManagerMenuMessages.java
+++ view/enums/managermenu/ManagerMenuMessages.java
@@ -1,4 +1,21 @@
 package view.enums.managermenu;
 
 public enum ManagerMenuMessages {
+    RESTAURANT_ADDED(""),
+    RESTAURANT_EXISTS(""),
+    RESTAURANT_NOT_FOUND(""),
+    RESTAURANT_REMOVED(""),
+    RESTAURANT_OPENED(""),
+    NO_RESTAURANTS(""),
+    ;
+
+    private String message;
+
+    ManagerMenuMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
