b48f52a43d434161402de85540db32882d7f94b3
sajjad hashem <sajjad.hashembeigi80@gmail.com>
2023-07-11 12:25:15 +0330

    commit #01

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("afterLogin.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("userMainScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 620);
         stage.setTitle("SUTFOOD!");
         stage.setScene(scene);
