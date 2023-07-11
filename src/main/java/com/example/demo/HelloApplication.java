471e9ff48821bf668e1a33877fc84944c454265f
sajjad hashem <sajjad.hashembeigi80@gmail.com>
2023-07-11 21:59:45 +0330

    commit #06:userRestaurantInfoScene fxml implemented.

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("userRestaurantScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("userFoodScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 620);
         stage.setTitle("SUTFOOD!");
         stage.setScene(scene);
