e54704acd7a3aa0c5c91d05d8f55996134f23447
Amir Hossein Saririan <saririan.amir@gmail.com>
2023-07-12 00:42:23 +0330

    Final Graph and Map

---
+++ Final Map/firstPhase1.2/src/MainMap.java
@@ -0,0 +1,40 @@
+public class MainMap {
+    public static void main(String[] args) {
+
+        // Input
+        // Source & Destination is the index of the nodes (from 1 to 1000)
+        // مبدا و مقصد شماره راس مربوطه در گراف است از 1 تا 1000
+        int source=258;
+        int destination=349;
+        int[] shortestRouteFound;
+
+        //The output is an int array with a length of (number of nodes in path + 1)
+        //The last index of the output array is the distance of path
+
+        shortestRouteFound = ShortestRoute.runPathFinding(source,destination);
+
+        //if you want distance/time use this
+        //shortestRouteFound[shortestRouteFound.length]
+
+        //if you want path use this
+        //shortestRouteFound[0] to shortestRouteFound[shortestRouteFound.length-1]
+
+        
+
+
+
+        //Printing output just in case
+
+//        if (shortestRouteFound.length == 0) {
+//            System.out.println("No path found from node " + sourcePlusOne + " to node " + destinationPlusOne);
+//        } else {
+//            System.out.print("Shortest path from node " + source + " to node " + destination + ": ");
+//            for (int c=0; c<shortestRouteFound.length-1; c++) {
+//                System.out.print(shortestRouteFound[c]+1);
+//                if(c!=shortestRouteFound.length-2) System.out.print(" ---> ");
+//            }
+//            System.out.println();
+//            System.out.println("And the distance is: " + shortestRouteFound[shortestRouteFound.length-1]);
+//        }
+    }
+}
---
+++ Final Map/firstPhase1.2/src/ShortestRoute.java
@@ -0,0 +1,104 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class ShortestRoute {
+    public static int[] findShortestPath(Graph graph, int source, int destination) {
+        int numNodes = graph.getNodesNum();
+        int[] distances = new int[numNodes];
+        int[] previous = new int[numNodes];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        Arrays.fill(previous, -1);
+
+        distances[source] = 0;
+
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(source, 0));
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            if (current.id == destination) {
+                break;
+            }
+
+            for (int neighbor = 0; neighbor < numNodes; neighbor++) {
+                int weight = graph.matrix[current.id][neighbor];
+                if (weight > 0) {
+                    int distance = current.distance + weight;
+                    if (distance < distances[neighbor]) {
+                        distances[neighbor] = distance;
+                        previous[neighbor] = current.id;
+                        queue.add(new Node(neighbor, distance));
+                    }
+                }
+            }
+        }
+
+        if (distances[destination] == Integer.MAX_VALUE) {
+            return new int[0]; // No path found
+        }
+
+        // Reconstruct the shortest path
+        List<Integer> route = new ArrayList<>();
+        int currentNId = destination;
+        while (currentNId != -1) {
+            route.add(currentNId);
+            currentNId = previous[currentNId];
+        }
+
+        // Reverse the path
+        Collections.reverse(route);
+
+        // Convert the path to an array
+        int[] shortestRoute = new int[route.size()+1];
+        for (int i = 0; i < route.size(); i++) {
+            shortestRoute[i] = route.get(i);
+        }
+
+        // Adding Distance
+        shortestRoute[route.size()] = distances[destination];
+
+        return shortestRoute;
+    }
+
+    public static int[] runPathFinding (int sourcePlusOne, int destinationPlusOne) {
+
+        Graph graph=new Graph();
+        int n,m,i,j,w;
+
+        try {
+            File graphFile = new File("./src/graph.txt");
+            Scanner input = new Scanner(graphFile);
+            int f=1;
+            while (input.hasNextLine()) {
+                if(f==1){
+                    n=input.nextInt();
+                    m=input.nextInt();
+                    graph=new Graph(n,m);
+                    f=0;
+                }
+                else {
+                    i=input.nextInt();
+                    j=input.nextInt();
+                    w=input.nextInt();
+                    graph.setEdge(i-1,j-1,w);
+                    graph.setEdge(j-1,i-1,w);
+                }
+            }
+            input.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred reading graph file.");
+            e.printStackTrace();
+        }
+
+//        int sourcePlusOne = 258;
+//        int destinationPlusOne = 349;
+        int source=sourcePlusOne-1;
+        int destination=destinationPlusOne-1;
+
+        return findShortestPath(graph, source, destination);
+    }
+}
+
